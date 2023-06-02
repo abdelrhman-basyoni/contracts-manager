a0f0a3fdf12e5f2e913f19df3f8a43d0462cd754
Abdelrhman Basyoni <64443028+abdelrhman-basyoni@users.noreply.github.com>
2023-06-02 22:57:26 +0300

    adding custom errors

---
+++ src/core/domain/utils/Errors.ts
@@ -0,0 +1,7 @@
+export class PermissionError extends Error {}
+
+export class NotFoundError extends Error {}
+
+export class ValidationError extends Error {}
+
+export class DatabaseError extends Error {}
