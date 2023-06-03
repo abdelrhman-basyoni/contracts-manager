89cbd83384399a45bb78b5aa9e59bf212ba91fd0
Abdelrhman Basyoni <64443028+abdelrhman-basyoni@users.noreply.github.com>
2023-06-03 22:48:16 +0300

    Create Contract.ts

---
+++ src/core/domain/entities/Contract.ts
@@ -0,0 +1,8 @@
+export class Contract {
+  constructor(
+    readonly ContractID: string,
+    readonly userID: string,
+    readonly contractName: string,
+    readonly templateID: string,
+  ) {}
+}
