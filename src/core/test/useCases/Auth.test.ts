7eb5499b3140c3b8cbd0488f2d7c964c11850a26
Abdelrhman Basyoni <64443028+abdelrhman-basyoni@users.noreply.github.com>
2023-06-03 15:02:54 +0300

    adding test for register

--- src/core/test/useCases/Auth.test.ts
+++ src/core/test/useCases/Auth.test.ts
@@ -7,42 +7,73 @@ import { BcryptPasswordService } from 'src/core/implementation/services/Password
 import { JsonWebTokenService } from 'src/core/implementation/services/Token';
 import { mock, instance, when } from 'ts-mockito';
 
-describe(' login useCase', () => {
+describe('login useCase', () => {
   let userRepo: UserRepository;
   let useCase: AuthUseCase;
-  let passwordService: PasswordService;
-
-  beforeAll(async () => {
+  const passwordService: PasswordService = new BcryptPasswordService();
+  const tokenService = new JsonWebTokenService();
+  beforeEach(() => {
     userRepo = mock<DynamoUserRepository>();
-    passwordService = new BcryptPasswordService();
-    useCase = new AuthUseCase(instance(userRepo), new JsonWebTokenService(), passwordService);
-    const username = 'username';
-    const password = 'password';
-    const hashedPassword = await passwordService.hashPassword(password);
-    const user = new User(username, hashedPassword);
-    when(userRepo.findOneByUsername(username))
-      .thenResolve(user)
-      .thenResolve(user)
-      .thenResolve(null);
+
+    useCase = new AuthUseCase(instance(userRepo), tokenService, passwordService);
   });
 
   it('should return accessToken token as a result of successful login', async () => {
     const username = 'username';
     const password = 'password';
+    const hashedPassword = await passwordService.hashPassword(password);
+    const user = new User(username, hashedPassword);
+    when(userRepo.findOneByUsername(username)).thenResolve(user);
 
     expect(useCase.login(username, password)).resolves.toHaveProperty('accessToken');
   });
 
   it('should throw error invalid login  when given a wrong password', async () => {
     const username = 'username';
+    const password = 'password';
+    const hashedPassword = await passwordService.hashPassword(password);
+    const user = new User(username, hashedPassword);
 
+    when(userRepo.findOneByUsername(username)).thenResolve(user);
     expect(useCase.login(username, 'wrong password')).rejects.toThrow();
   });
 
   it('should throw error invalid login  when given a wrong username', async () => {
     const username = 'username';
     const password = 'password';
+    when(userRepo.findOneByUsername(username)).thenResolve(null);
 
     expect(useCase.login(username, password)).rejects.toThrow();
+  });
+});
+
+describe('register useCase', () => {
+  let userRepo: UserRepository;
+  let useCase: AuthUseCase;
+  const passwordService: PasswordService = new BcryptPasswordService();
+  const tokenService = new JsonWebTokenService();
+
+  beforeEach(() => {
+    userRepo = mock<DynamoUserRepository>();
+    useCase = new AuthUseCase(instance(userRepo), tokenService, passwordService);
+  });
+
+  it('should successfully register a new user', async () => {
+    const username = 'newUser';
+    const password = 'newPassword';
+    when(userRepo.findOneByUsername(username)).thenResolve(null);
+
+    when(userRepo.registerUser(username, 'hashedPassword')).thenResolve();
+
+    await expect(useCase.register(username, password)).resolves.not.toThrow();
+  });
+
+  it('should throw an error when trying to register an existing user', async () => {
+    const username = 'existingUser';
+    const password = 'newPassword';
+    const existingUser = new User(username, 'hashedPassword');
+    when(userRepo.findOneByUsername(username)).thenResolve(existingUser);
+
+    await expect(useCase.register(username, password)).rejects.toThrow();
   });
 });
